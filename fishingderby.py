d53bb0905f129bc62e45fa6b7057bce9d46eb2c0
Quinn <quinnclmn15@gmail.com>
2018-12-01 20:45:54 -0800

    Shark dimensions

--- fishingderby.py
+++ fishingderby.py
@@ -7,20 +7,23 @@ def main():
     env = gym.make('FishingDerby-v0')
     env.reset()
    
-    env.render()
-    obs, rew, done, info = env.step(env.action_space.sample()) # take a random action
-    while not done:
+    # env.render()
+    # obs, rew, done, info = env.step(env.action_space.sample()) # take a random action
+    while True:
         env.render()
         obs, rew, done, info = env.step(env.action_space.sample())
         time.sleep(0.01)
+        if done:
+            time.sleep(2)
+            env.reset()
 
-    time.sleep(10)
+    # 
 
     # Image parsing alg information:
     # Fish:     color: 232, 232, 74     dim: 10 x 16 pix 
     # Borders (lft/rht):                dim: 8 pix
     # Pier                              dim: 20 pix (pole 4 pix)
-    # 
+    # Shark                             80 to 92 (13 pix height)
 
 
 
