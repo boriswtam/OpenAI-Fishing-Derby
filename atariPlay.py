b670a4be131d273607228f9e1a6b162ebab41a2f
Boris Tam <boris22t@gmail.com>
2018-12-10 01:10:16 -0800

    Boris's files

---
+++ atariPlay.py
@@ -0,0 +1,41 @@
+# INITIALIZATION: libraries, parameters, network...
+
+from keras.models import Sequential      # One layer after the other
+from keras.layers import Dense, Flatten  # Dense layers are fully connected layers, Flatten layers flatten out multidimensional inputs
+from collections import deque            # For storing moves 
+from keras.models import model_from_json
+import numpy as np
+import gym                                # To train our network
+env = gym.make('Boxing-v0')          # Choose game (any in the gym should work)
+
+import random     # For sampling batches from the observations
+
+# load json and create model
+json_file = open('model.json', 'r')
+model_json = json_file.read()
+json_file.close()
+model = model_from_json(model_json)
+# load weights into new model
+model.load_weights("model.h5")
+print("Loaded model from disk")
+
+
+
+print("\nenv", env.observation_space.shape)
+print(env.action_space.n)
+print(env.action_space.shape)
+observation = env.reset()
+print(observation.shape)
+obs = np.expand_dims(observation, axis=0)
+state = np.stack((obs, obs), axis=1)
+done = False
+tot_reward = 0.0
+while not done:
+    env.render()                    # Uncomment to see game running
+    Q = model.predict(state)        
+    action = np.argmax(Q)         
+    observation, reward, done, info = env.step(action)
+    obs = np.expand_dims(observation, axis=0)
+    state = np.append(np.expand_dims(obs, axis=0), state[:, :1, :], axis=1)    
+    tot_reward += reward
+print('Game ended! Total reward: {}'.format(reward))
