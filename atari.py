8149df5994fb82bcdcce26fc06e3df93b8833192
jhuang81 <jhuang81@235x30.ad.calpoly.edu>
2018-12-11 22:11:22 -0800

    cleaned up

--- atari.py
+++ atari.py
@@ -1,41 +1,22 @@
-from keras.models import Sequential      # One layer after the other
-from keras.layers import *  # Dense layers are fully connected layers, Flatten layers flatten out multidimensional inputs
-from collections import deque            # For storing moves 
+from keras.models import Sequential
+from keras.layers import *
+from collections import deque
 from keras.models import model_from_json
 import numpy as np
-import gym                                # To train our network
-env = gym.make('FishingDerby-v0')         # Load fishing derby
+import gym
+import random
 
-import random     # For sampling batches from the observations
-
+env = gym.make('FishingDerby-v0')
 #env: (210, 160, 3)
-# Create network. Input is two consecutive game states, output is Q-values of the possible moves.
 model = Sequential()
-'''
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=  env.observation_space.shape))
-#model.add(MaxPooling2D(pool_size=(3, 3)))
-model.add(Conv2D(16, (3, 3), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(3, 3)))
-#model.add(Conv2D(64, (3, 3), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(3, 3)))
-model.add(Flatten())
-model.add(Dense(9, activation='relu'))
-model.add(Dense(env.action_space.n, init='uniform', activation='linear'))    # Same number of outputs as possible actions
-print("action space", env.action_space.n)
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-'''
 
 actions = 10
 model.add(Conv2D(32, kernel_size=(8, 8), strides= 4, activation='relu', input_shape=  env.observation_space.shape))
-#model.add(MaxPooling2D(pool_size=(3, 3)))
 model.add(Conv2D(64, (4, 4), strides=2, activation='relu'))
 model.add(Conv2D(64, (3, 3), strides=1, activation='relu'))
-#model.add(MaxPooling2D(pool_size=(3, 3)))
-#model.add(Conv2D(64, (3, 3), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(3, 3)))
 model.add(Flatten())
 model.add(Dense(512, activation='relu'))
-model.add(Dense(actions, init='uniform', activation='linear'))    # Same number of outputs as possible actions
+model.add(Dense(actions, init='uniform', activation='linear'))
 print("action space", actions)
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
@@ -43,33 +24,20 @@ print("input", env.observation_space.shape)
 for layer in model.layers:
     print("layer", layer.output_shape)
 
-# Pixels to info/objects to moves
-
-'''
-model.add(Dense(20, input_shape=(2,) + env.observation_space.shape, init='uniform', activation='relu'))
-model.add(Flatten())       # Flatten input so as to have no problems with processing
-model.add(Dense(18, init='uniform', activation='relu'))
-model.add(Dense(10, init='uniform', activation='relu'))
-model.add(Dense(env.action_space.n, init='uniform', activation='linear'))    # Same number of outputs as possible actions
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-#model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-'''
-
-# Parameters
-D = deque()                                # Register where the actions will be stored
-
-observetime = 1500                 # Number of timesteps we will be acting on the game and observing results
-epsilon = 0.9                              # Probability of doing a random move
-#1: long term, 0: short term
-gamma = 0.5                              # Discounting factor for future reward. How much we care about steps further in time
-mb_size = 100                         # Learning minibatch size
-
-# Perform experience replay - by storing play data (observing)
+D = deque()
+# Parameters to edit
+#time spent observing
+observetime = 10000
+# Probability of doging a random move
+epsilon = 0.99
+# discounting factor
+gamma = 0.55
+# learning batch size
+mb_size = 200
 
 # FIRST STEP: Knowing what each action does (Observing)
-#get terms, frequency, get n gram; go through list and translate to freq; create new entry with document key
-observation = env.reset()                     # Game begins
-obs = np.expand_dims(observation, axis=0)     # (Formatting issues) Making the observation the first element of a batch of inputs 
+observation = env.reset()
+obs = np.expand_dims(observation, axis=0)
 print("obs", obs.shape)
 state = np.stack((obs), axis=0)
 print("state", state.shape)
@@ -78,41 +46,27 @@ for t in range(observetime):
     #env.render()
     if np.random.rand() <= epsilon:
         action = np.random.randint(0, actions, size=1)[0]
-        print("action", action)
     else:
-        Q = model.predict(state)         # Q-values predictions
-        #print("predictions", Q.shape)
-        action = np.argmax(Q)             # Move with highest Q-value is the chosen one
-        #print("action", action)
+        Q = model.predict(state)
+        action = np.argmax(Q)
 
-    observation_new, reward, done, info = env.step(action)     # See state of the game, reward... after performing the action
-    #print("new obs", observation.shape)
-    obs_new = np.expand_dims(observation_new, axis=0)          # (Formatting issues)
-    #print("new obs 2", obs_new.shape)
-    #state_new = np.append(np.expand_dims(obs_new, axis=0), state[:, :1, :], axis=1)     # Update the input with the new state of the game
+    observation_new, reward, done, info = env.step(action)
+    obs_new = np.expand_dims(observation_new, axis=0)
     state_new = obs_new[:]
-    #print("state new", state_new.shape)
-    
-    # D holds tuples of experience data
-    D.append((state, action, reward, state_new, done))         # 'Remember' action and consequence
-    state = state_new         # Update state
+
+    D.append((state, action, reward, state_new, done))
+    state = state_new
     if done:
-        env.reset()           # Restart game if it's finished
-        obs = np.expand_dims(observation, axis=0)     # (Formatting issues) Making the observation the first element of a batch of inputs 
+        env.reset()
+        obs = np.expand_dims(observation, axis=0)
         state = np.stack((obs), axis=0)
 print('Observing Finished')
 
 # SECOND STEP: Learning from the observations (Experience replay)
-
-minibatch = random.sample(D, mb_size)                              # Sample some moves
-# Minibatch is a tuple or experience data
-
-# initialize space
+minibatch = random.sample(list(D), mb_size)
 inputs_shape = (mb_size,) + state.shape[1:]
 inputs = np.zeros(inputs_shape)
 targets = np.zeros((mb_size, actions))
-# targets = np.zeros((mb_size, env.action_space.n))
-
 
 for i in range(0, mb_size):
     print("learning", i)
@@ -121,25 +75,19 @@ for i in range(0, mb_size):
     reward = minibatch[i][2]
     state_new = minibatch[i][3]
     done = minibatch[i][4]
-    
+
     # Build Bellman equation for the Q function
     inputs[i:i+1] = np.expand_dims(state, axis=0)
     targets[i] = model.predict(state)
-    
-    # Approximate value function?
     Q_sa = model.predict(state_new)
-    # Q_sa and targets contains numpy array(s) of predictions
 
     if done:
         targets[i, action] = reward
     else:
-        # (Discounting factor & max prediction of Approx. val func) added to reward
         targets[i, action] = reward + gamma * np.max(Q_sa)
 
-    # Train network to output the Q function
-    # Inputs are the state (frame), and target data
     model.train_on_batch(inputs, targets)
-    if i % 10 == 0:
+    if i % 100 == 0:
         # serialize model to JSON
         model_json = model.to_json()
         file = "model" + str(i) + ".json"
@@ -159,4 +107,3 @@ with open("model.json", "w") as json_file:
 # serialize weights to HDF5
 model.save_weights("model.h5")
 print("Saved model to disk")
-
